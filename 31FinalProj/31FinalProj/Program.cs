e7e7c7a5ee2891328041476cd05ca3b1ad4d0c2f
grommitt <gmshaughn@email.arizona.edu>
2017-06-09 16:05:26 -0700

    first

---
+++ 31FinalProj/31FinalProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _31FinalProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
